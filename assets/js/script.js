5feb608398a5c555571a360e9851d35f05254ed1
Joshua Taylor <toasterrage@gmail.com>
2022-11-22 21:50:50 -0700

    Updated JS with starter api code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,27 @@ const globalFunc = {
     // Retrieve user search history from localStorage function
     saveLocal: function() {
         localStorage.setItem('trailPlanner', JSON.stringify(searchHistoryObj));
+    },
+    // Use the location address or name as a parameter when calling getLocation()
+    getLocation: function(loc) {
+        let geocodingUrl = `https://api.geoapify.com/v1/geocode/search?name=${loc}&format=json&apiKey=${geoapifyApiKey}`;
+
+        // Fetch lat/lon data from geoapify API
+        fetch(geocodingUrl, {method: 'GET'})
+            .then(response => {
+                console.log(response);
+                return response.json();})
+            .then(data => {
+
+                console.log(data);
+
+            })
+        //
     }
 }
 
 // *****Global Variables*****
+const geoapifyApiKey = '035e16b84ace4340b1c953b2f690fc7e';
 /* An example of the data stored in the searchHistoryObj
 {
     id: {
@@ -31,3 +48,10 @@ const searchHistoryObj = (localStorage['trailPlanner']) ? globalFunc.loadLocal()
 //searchHistoryObj['id2'] = {name: 'test2 name'};
 console.log(searchHistoryObj);
 */
+
+// REMOVE LATER - Testing geocoding api
+globalFunc.getLocation('carpenter peak trail colorado');
+
+
+// TO DO - Set up autocomplete from geoapify address autocomplete API
+// TO DO - Use bbox get location map from openstreetmaps API
